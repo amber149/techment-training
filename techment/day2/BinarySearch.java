a232038478ce6f9b1ee9cde66ee7e50b97c17a69
amber149 <47914741+amber149@users.noreply.github.com>
2020-06-04 13:18:21 +0530

    Add files via upload

---
+++ techment/day2/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.techment.day2;
+import java.util.Scanner;
+public class BinarySearch {
+	public static void main(String[] args) {
+		int a[]= {12, 23,29,32,36,45,56,69};
+		int num, beg, end, mid;
+		boolean found=false;
+		beg=0;
+		end=a.length-1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number ");
+		num=sc.nextInt();
+		while(beg<=end)
+		{
+			mid=(beg+end)/2;
+			if(a[mid]==num)
+			{
+				System.out.println("Found at position "+(mid+1));
+				found=true;
+				break;
+			}
+			else if(num>a[mid])
+				beg=mid+1;
+			else if(num<a[mid])
+				end=mid-1;
+		}
+		if(found==false)
+			System.out.println("No. not found ");
+		
+
+	}
+
+}
+	
+
